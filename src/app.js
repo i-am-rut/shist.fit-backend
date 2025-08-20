7d7b0b882d36c1ce9317d33049223aedf6ab551a
Amrut Patil <patilamrut578@gmail.com>
2025-08-20 16:51:04 +0530

    Add register, login and logout functionality

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const connectToDB = require("./config/database")
+const cookieParser = require("cookie-parser")
+const authRoutes = require("./routes/authRoutes")
 
 dotenv.config()
 const app = express()
+
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use('/auth', authRoutes)
 
 
 connectToDB()
---
+++ src/utils/validate.js
@@ -0,0 +1,14 @@
+const validator = require("validator")
+
+const validateSignUpData = (req) => {
+    const {name, email, password} = req.body
+    if(!name || name.length < 2) {
+        throw new Error("Name is not valid.")
+    } else if (!validator.isEmail(email)) {
+        throw new Error("Email is not valid.");
+    } else if (!validator.isStrongPassword(password)) {
+        throw new Error("Please enter a strong password!");
+    }
+}
+
+module.exports = {validateSignUpData}
