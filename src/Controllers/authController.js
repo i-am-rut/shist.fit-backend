ecee616605167e4494159bafc12c7e7d1d4fd9b9
Amrut Patil <patilamrut578@gmail.com>
2025-08-21 16:59:46 +0530

    Refactor auth to read cookies

--- src/Controllers/authController.js
+++ src/Controllers/authController.js
@@ -28,7 +28,7 @@ const login = async(req, res) => {
     try {
         const {email, password} = req.body
         const user = await User.findOne({email})
-        // console.log(user)
+
         if(!user) return res.status(400).json({message: "Invalid credentials!"})
         // if(!user.isEmailVerified) return res.status(403).json({message: "Verify your email from the link sent to your email address to login."})
 
@@ -37,10 +37,17 @@ const login = async(req, res) => {
         if(isPasswordValid) {
             const token = await user.getJWT()
 
-            res.cookie("token", token, {expires: new Date(Date.now() + 8 * 3600000),})
-            res.status(200).json({
+            res.cookie("token", token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                maxAge: 7 * 24 * 60 * 60 * 1000
+            }).status(200).json({
                 message: "Login successful",
-                user: {id: user._id, name: user.name, email: user.email}
+                user: {
+                    id: user._id, 
+                    name: user.name, 
+                    email: user.email
+                }
             })
         }else {
             throw new Error("Invalid credentials!");
@@ -53,8 +60,9 @@ const login = async(req, res) => {
 
 const logout = async(req, res) => {
     try {
-        res.cookie("token", null, {
-            expires: new Date(Date.now()),
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production'
         })
         res.status(200).json({message: "Logged out successfully!"})    
     } catch (err) {
